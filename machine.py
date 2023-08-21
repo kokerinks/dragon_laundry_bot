075ac43ca5d0960a07ade07a978ebddd5a1f1457
davidgohzk <david.gohzhekai@gmail.com>
2023-08-21 08:51:01 +0800

    Refactored code to be more pythonic and less cancer

--- machine.py
+++ machine.py
@@ -1,66 +1,58 @@
 from abc import ABC
 import datetime
-from math import floor
 import pytz
 
 sgt_timezone = pytz.timezone("Asia/Singapore")
 
 
 class Machine(ABC):
-    isAvailable = True
+    COMPLETION_TEXT = "Fuyohhhhhh!! Your clothes are ready for collection! Please collect them now so that others may use it"
 
-    completionText = "Fuyohhhhhh!! Your clothes are ready for collection! Please collect them now so that others may use it"
+    # constant value which stores total time required for start (IN SECONDS)
+    is_available = True
+    time_to_complete = None
+    # datetime storing start time of machine
+    end_time = None
+    curr_user = ""
 
-    ##constant value which stores total time required for start (IN SECONDS)
-    timeToComplete = None
+    def __init__(self, new_time_to_complete, new_name):
+        self.time_to_complete = new_time_to_complete
+        self.name = new_name
 
-    ##datetime storing start time of machine
-    endTime = None
-
-    currUser = ""
-
-    def __init__(self, newTimeToComplete, newName):
-        self.timeToComplete = newTimeToComplete
-        self.name = newName
-
-    def getName(self):
+    def get_name(self):
         return self.name
 
-    def getTimeToComplete(self):
-        return self.timeToComplete
+    def get_time_to_complete(self):
+        return self.time_to_complete
 
     def status(self):
-        if self.isAvailable:
+        if self.is_available:
             reply = f"AVAILABLE \U00002705"
-            if self.currUser:
-                reply += f', last used by @{self.currUser} ({self.endTime.astimezone(sgt_timezone).strftime("%d/%m/%Y %I:%M%p")})'
+            if self.curr_user:
+                reply += f', last used by @{self.curr_user} ({self.end_time.astimezone(sgt_timezone).strftime("%d/%m/%Y %I:%M%p")})'
             return reply
-        else:
-            timeDelta = self.endTime - datetime.datetime.now()
-            timeInMin = timeDelta.seconds // 60
-            timeInSec = timeDelta.seconds % 60
-            return f"UNAVAILABLE \U0000274C for {timeInMin}mins and {timeInSec}s by @{self.currUser}"
+        time_delta = self.end_time - datetime.datetime.now()
+        time_in_min = time_delta.seconds // 60
+        time_in_sec = time_delta.seconds % 60
+        return f"UNAVAILABLE \U0000274C for {time_in_min}mins and {time_in_sec}s by @{self.curr_user}"
 
     def time_left_mins(self):
-        return self.timeToComplete // 60
+        return self.time_to_complete // 60
 
     def time_left_secs(self):
-        return self.timeToComplete % 60
+        return self.time_to_complete % 60
 
-    def total_time(self):
-        return f"{self.time_left_mins()}mins"
-
-    def start_machine(self, newUser):
-        if not (self.isAvailable):
+    def start_machine(self, new_user):
+        if not self.is_available:
             return False
-        else:
-            self.isAvailable = False
-            self.endTime = datetime.datetime.now() + datetime.timedelta(
-                seconds=self.timeToComplete
-            )
-            self.currUser = newUser
-            return True
+
+        self.is_available = False
+        self.end_time = datetime.datetime.now() + datetime.timedelta(
+            seconds=self.time_to_complete
+        )
+        self.curr_user = new_user
+        return True
 
     def alarm(self):
-        self.isAvailable = True
-        return self.completionText
+        self.is_available = True
+        return self.COMPLETION_TEXT
--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-import datetime
-import random
-import re
-from pytz import timezone
 import telegram
-from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
-import logging
-from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler, ConversationHandler, Filters, MessageHandler
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    CallbackContext,
+    CallbackQueryHandler,
+    ConversationHandler,
+)
 from machine import Machine
 
 
+API_KEY = "6664557635:AAHekMqFEWI1lC8C5RSGTUoIYb7OANIEwLY"
 MENU = 1
 
-Tbot = telegram.Bot("6664557635:AAHekMqFEWI1lC8C5RSGTUoIYb7OANIEwLY")
+TBOT = telegram.Bot(API_KEY)
 
-washertimer = 32*60
-dryertimer = 32*60
-WASHER_ONE = Machine(washertimer, "Washer ONE")
-WASHER_TWO = Machine(washertimer, "Washer TWO")
-DRYER_ONE = Machine(dryertimer, "Dryer ONE")
-DRYER_TWO = Machine(dryertimer, "Dryer TWO")
+WASHER_TIMER = 32 * 60
+DRYER_TIMER = 32 * 60
+DRYER_ONE = Machine(DRYER_TIMER, "DRYER ONE")
+DRYER_TWO = Machine(DRYER_TIMER, "DRYER TWO")
+WASHER_ONE = Machine(WASHER_TIMER, "WASHER ONE")
+WASHER_TWO = Machine(WASHER_TIMER, "WASHER TWO")
 
-all_machines = [DRYER_ONE, DRYER_TWO, WASHER_ONE, WASHER_TWO]
 
 def main():
-    updater = Updater("6664557635:AAHekMqFEWI1lC8C5RSGTUoIYb7OANIEwLY")
-    # Test Laundry Bot
-    # updater = Updater("5480884899:AAH5QJV9TL4Ls9DxJzFZwCEvJcfqWxiAwpc")
-
+    updater = Updater(API_KEY)
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
@@ -36,50 +38,43 @@ def main():
     # $ means "end of line/string"
     # So ^ABC$ will only allow 'ABC'
     conv_handler = ConversationHandler(
-    entry_points=[
-      CommandHandler('start', start),
-      CommandHandler("select", select),
-      CommandHandler("status", status),
-##      MessageHandler(
-##        ~Filters.command & Filters.regex(re.compile(r"done", re.IGNORECASE)),
-##        handle_message)
-    ],
-    states={
-      MENU: [
-        CallbackQueryHandler(cancel, pattern='^' + 'exit' + '$'),
-        CallbackQueryHandler(cancel, pattern='^' + 'exits' + '$'),
-        #
-        # # which callback_data does start get call
-        CallbackQueryHandler(double_confirm_dryer_one_callback,
-                             pattern='^' + 'dryer_one' + '$'),
-        CallbackQueryHandler(double_confirm_washer_one_callback,
-                             pattern='^' + 'washer_one' + '$'),
-        CallbackQueryHandler(double_confirm_dryer_two_callback,
-                             pattern='^' + 'dryer_two' + '$'),
-        CallbackQueryHandler(double_confirm_washer_two_callback,
-                             pattern='^' + 'washer_two' + '$'),
-        CallbackQueryHandler(backtomenu, pattern='^' + 'no_dryer_one' + '$'),
-        CallbackQueryHandler(backtomenu, pattern='^' + 'no_washer_one' + '$'),
-        CallbackQueryHandler(backtomenu, pattern='^' + 'no_dryer_two' + '$'),
-        CallbackQueryHandler(backtomenu, pattern='^' + 'no_washer_two' + '$'),
-        CallbackQueryHandler(set_timer_dryer_one,
-                             pattern='^' + 'yes_dryer_one' + '$'),
-        CallbackQueryHandler(set_timer_washer_one,
-                             pattern='^' + 'yes_washer_one' + '$'),
-        CallbackQueryHandler(set_timer_dryer_two,
-                             pattern='^' + 'yes_dryer_two' + '$'),
-        CallbackQueryHandler(set_timer_washer_two,
-                             pattern='^' + 'yes_washer_two' + '$'),
-      ]
-    },
-    fallbacks=[
-      CommandHandler('start', start),
-      CommandHandler("select", select),
-      CommandHandler("status", status),
-      # MessageHandler(
-      #   ~Filters.command & Filters.regex(re.compile(r"done", re.IGNORECASE)),
-      #   handle_message)
-    ],
+        entry_points=[
+            CommandHandler("start", start),
+            CommandHandler("select", select),
+            CommandHandler("status", status),
+        ],
+        states={
+            MENU: [
+                CallbackQueryHandler(cancel, pattern="^exit$"),
+                CallbackQueryHandler(cancel, pattern="^exits$"),
+                CallbackQueryHandler(
+                    double_confirm_dryer_one_callback, pattern="^dryer_one$"
+                ),
+                CallbackQueryHandler(
+                    double_confirm_dryer_two_callback, pattern="^dryer_two$"
+                ),
+                CallbackQueryHandler(
+                    double_confirm_washer_one_callback,
+                    pattern="^washer_one$",
+                ),
+                CallbackQueryHandler(
+                    double_confirm_washer_two_callback, pattern="^washer_two$"
+                ),
+                CallbackQueryHandler(backtomenu, pattern="^no_dryer_one$"),
+                CallbackQueryHandler(backtomenu, pattern="^no_dryer_two$"),
+                CallbackQueryHandler(backtomenu, pattern="^no_washer_one$"),
+                CallbackQueryHandler(backtomenu, pattern="^no_washer_two$"),
+                CallbackQueryHandler(set_timer_dryer_one, pattern="^yes_dryer_one$"),
+                CallbackQueryHandler(set_timer_dryer_two, pattern="^yes_dryer_two$"),
+                CallbackQueryHandler(set_timer_washer_one, pattern="^yes_washer_one$"),
+                CallbackQueryHandler(set_timer_washer_two, pattern="^yes_washer_two$"),
+            ]
+        },
+        fallbacks=[
+            CommandHandler("start", start),
+            CommandHandler("select", select),
+            CommandHandler("status", status),
+        ],
     )
 
     # Add ConversationHandler to dispatcher that will be used for handling updates
@@ -93,131 +88,145 @@ def main():
     # non-blocking and will stop the bot gracefully.
     updater.idle()
 
+
 def start(update: Update, context: CallbackContext) -> None:
-  # Don't allow users to use /start command in group chats
-  if update.message.chat.type != 'private':
-    Tbot.send_message(
-      chat_id=update.message.from_user.id,
-      text=
-      f"""Hi @{update.message.from_user.username},\n\nThanks for calling me in the groupchat. To prevent spamming in the group, please type /start to me privately in this chat instead!"""
+    # Don't allow users to use /start command in group chats
+    if update.message.chat.type != "private":
+        TBOT.send_message(
+            chat_id=update.message.from_user.id,
+            text=f"Hi @{update.message.from_user.username},\n\nThanks for calling me in the groupchat. To prevent spamming in the group, please type /start to me privately in this chat instead!",
+        )
+        return MENU
+
+    if len(context.args) > 0:
+        return
+
+    keyboard = [[InlineKeyboardButton("Exit", callback_data="exit")]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(
+        "Welcome to Dragon Laundry Bot!\U0001f600\U0001F606\U0001F923\n\nUse the following commands to use this bot:\n/select: Select the washer/dryer that you want to use\n/status: Check the status of Washers and Dryers\n\nThank you for using the bot!\nCredit to: @Kaijudo",
+        reply_markup=reply_markup,
     )
     return MENU
 
-  keyboard = [[InlineKeyboardButton('Exit', callback_data='exit')]]
-  if len(context.args) > 0:
-    return
-
-  reply_markup = InlineKeyboardMarkup(keyboard)
-  update.message.reply_text(
-    'Welcome to Dragon Laundry Bot!\U0001f600\U0001F606\U0001F923\n\nUse the following commands to use this bot:\n/select: Select the washer/dryer that you want to use\n/status: Check the status of Washers and Dryers\n\nThank you for using the bot!\nCredit to: @Kaijudo',
-    reply_markup=reply_markup)
-  return MENU
 
 def select(update: Update, context: CallbackContext) -> None:
-  # Don't allow users to use /select command in group chats
-  if update.message.chat.type != 'private':
+    # Don't allow users to use /select command in group chats
+    if update.message.chat.type != "private":
+        return MENU
+    keyboard = [
+        [
+            InlineKeyboardButton("Dryer One", callback_data="dryer_one"),
+            InlineKeyboardButton("Dryer Two", callback_data="dryer_two"),
+        ],
+        [
+            InlineKeyboardButton("Washer One", callback_data="washer_one"),
+            InlineKeyboardButton("Washer Two", callback_data="washer_two"),
+        ],
+        [InlineKeyboardButton("Exit", callback_data="exit")],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text(
+        "\U0001F606\U0001F923 Please choose a service: \U0001F606\U0001F923",
+        reply_markup=reply_markup,
+    )
     return MENU
-  keyboard = [[
-    InlineKeyboardButton('Dryer One', callback_data='dryer_one'),
-    InlineKeyboardButton('Dryer Two', callback_data='dryer_two')
-  ], [
-    InlineKeyboardButton('Washer One', callback_data='washer_one'),
-    InlineKeyboardButton('Washer Two', callback_data='washer_two'),
-              ], [InlineKeyboardButton('Exit', callback_data='exit')]]
 
-  reply_markup = InlineKeyboardMarkup(keyboard)
-
-  update.message.reply_text(
-    '\U0001F606\U0001F923 Please choose a service: \U0001F606\U0001F923',
-    reply_markup=reply_markup)
-  return MENU
 
 def cancel(update: Update, context: CallbackContext) -> int:
-  """Returns `ConversationHandler.END`, which tells the
-    ConversationHandler that the conversation is over"""
-  query = update.callback_query
-  query.answer()
-  query.edit_message_text(
-    text="Haiyaaa then you call me for what\n\nUse /start again to call me")
-  return ConversationHandler.END
+    """
+    Returns `ConversationHandler.END`, which tells the ConversationHandler that the conversation is over
+    """
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+        text="Haiyaaa then you call me for what\n\nUse /start again to call me"
+    )
+    return ConversationHandler.END
 
-def create_inline_for_callback(machineName):
-    callbackName = machineName.replace(" ", "_")
-    textName = machineName.upper()
-    keyboard = [[
-        InlineKeyboardButton('Yes', callback_data=f'yes_{callbackName}'),
-    ], [InlineKeyboardButton('No', callback_data=f'no_{callbackName}')]]
+
+def create_inline_for_callback(machine_name):
+    keyboard = [
+        [
+            InlineKeyboardButton("Yes", callback_data=f"yes_{machine_name}"),
+        ],
+        [InlineKeyboardButton("No", callback_data=f"no_{machine_name}")],
+    ]
     markup = InlineKeyboardMarkup(keyboard)
-    text = f"Timer for {textName} will begin?"
+    text = f"Timer for {machine_name.upper()} will begin?"
     return (text, markup)
 
-def double_confirm_dryer_one_callback(update: Update,
-                                     _: CallbackContext) -> int:
-  query = update.callback_query
-  query.answer()
-  text, markup = create_inline_for_callback("dryer one")
-  query.edit_message_text(text= text, reply_markup= markup)
-  return MENU
+
+def double_confirm_dryer_one_callback(update: Update, _: CallbackContext) -> int:
+    query = update.callback_query
+    query.answer()
+    text, markup = create_inline_for_callback("dryer_one")
+    query.edit_message_text(text=text, reply_markup=markup)
+    return MENU
 
 
-def double_confirm_washer_one_callback(update: Update,
-                                      _: CallbackContext) -> int:
-  query = update.callback_query
-  query.answer()
-  text, markup = create_inline_for_callback("washer one")
-  query.edit_message_text(text=text, reply_markup=markup)
-  return MENU
+def double_confirm_dryer_two_callback(update: Update, _: CallbackContext) -> int:
+    query = update.callback_query
+    query.answer()
+    text, markup = create_inline_for_callback("dryer_two")
+    query.edit_message_text(text=text, reply_markup=markup)
+    return MENU
 
 
-def double_confirm_dryer_two_callback(update: Update,
-                                     _: CallbackContext) -> int:
-  query = update.callback_query
-  query.answer()
-  text, markup = create_inline_for_callback("dryer two")
-  query.edit_message_text(text= text, reply_markup= markup)
-  return MENU
+def double_confirm_washer_one_callback(update: Update, _: CallbackContext) -> int:
+    query = update.callback_query
+    query.answer()
+    text, markup = create_inline_for_callback("washer_one")
+    query.edit_message_text(text=text, reply_markup=markup)
+    return MENU
 
-def double_confirm_washer_two_callback(update: Update,
-                                     _: CallbackContext) -> int:
-  query = update.callback_query
-  query.answer()
-  text, markup = create_inline_for_callback("washer two")
-  query.edit_message_text(text= text, reply_markup= markup)
-  return MENU
+
+def double_confirm_washer_two_callback(update: Update, _: CallbackContext) -> int:
+    query = update.callback_query
+    query.answer()
+    text, markup = create_inline_for_callback("washer_two")
+    query.edit_message_text(text=text, reply_markup=markup)
+    return MENU
+
 
 def backtomenu(update: Update, context: CallbackContext) -> None:
-  query = update.callback_query
-  query.answer()
-  keyboard = [[InlineKeyboardButton('Exit', callback_data='exits')]]
+    query = update.callback_query
+    query.answer()
+    keyboard = [[InlineKeyboardButton("Exit", callback_data="exits")]]
 
-  reply_markup = InlineKeyboardMarkup(keyboard)
-  query.edit_message_text(
-    'Welcome to Dragon Laundry Bot!\n\nUse the following commands to use this bot:\n/select: Select the washer/dryer that you want to use\n/status: Check the status of Washers and Dryers\n\nThank you for using the bot!\nCredit to: @Kaijudo',
-    reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        "Welcome to Dragon Laundry Bot!\n\nUse the following commands to use this bot:\n/select: Select the washer/dryer that you want to use\n/status: Check the status of Washers and Dryers\n\nThank you for using the bot!\nCredit to: @Kaijudo",
+        reply_markup=reply_markup,
+    )
+
 
 def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
-  """Remove job with given name. Returns whether job was removed."""
-  current_jobs = context.job_queue.get_jobs_by_name(name)
-  if not current_jobs:
-    return False
-  for job in current_jobs:
-    job.schedule_removal()
-  return True
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
 
 def alarm(context: CallbackContext, machine) -> None:
     """Send the alarm message."""
     job = context.job
     context.bot.send_message(
         job.context,
-        text= 'Fuyohhhhhh!! Your clothes are ready for collection! Please collect them now so that others may use it'
+        text="Fuyohhhhhh!! Your clothes are ready for collection! Please collect them now so that others may use it",
     )
     machine.alarm()
 
 
 def set_timer(update, context, machine):
-    machineName = machine.getName()
-    underscoreName = machineName.lower().replace(" ", "_")
-    upperName = machineName.upper()
+    machine_name = machine.get_name()
+    underscore_name = machine_name.lower().replace(" ", "_")
+    upper_name = machine_name.upper()
 
     """Add a job to the queue."""
     chat_id = update.effective_message.chat_id
@@ -227,18 +236,20 @@ def set_timer(update, context, machine):
     job_removed = remove_job_if_exists(str(chat_id), context)
 
     if not (machine.start_machine(update.effective_message.chat.username)):
-        text = f"{upperName} is currently in use. Please come back again later!"
+        text = f"{upper_name} is currently in use. Please come back again later!"
         query.message.delete()
-        Tbot.send_message(chat_id=chat_id, text=text)
+        TBOT.send_message(chat_id=chat_id, text=text)
     else:
-        context.job_queue.run_once(lambda context: alarm(context, machine), machine.getTimeToComplete(),
-                                   context=chat_id, name=underscoreName)
+        context.job_queue.run_once(
+            lambda context: alarm(context, machine),
+            machine.get_time_to_complete(),
+            context=chat_id,
+            name=underscore_name,
+        )
 
-    text = f"Timer Set for {machine.total_time()} for {upperName}. Please come back again!"
-    # if job_removed:
-    #    text = 'Status Update: QR DRYER is available'
+    text = f"Timer Set for {machine.time_left_mins()}mins for {upper_name}. Please come back again!"
     query.message.delete()
-    Tbot.send_message(chat_id=chat_id, text=text)
+    TBOT.send_message(chat_id=chat_id, text=text)
     return MENU
 
 
@@ -246,26 +257,25 @@ def set_timer_dryer_one(update: Update, context: CallbackContext) -> None:
     set_timer(update, context, DRYER_ONE)
 
 
-def set_timer_washer_one(update: Update, context: CallbackContext) -> None:
-    set_timer(update, context, WASHER_ONE)
-
-
 def set_timer_dryer_two(update: Update, context: CallbackContext) -> None:
     set_timer(update, context, DRYER_TWO)
+
+
+def set_timer_washer_one(update: Update, context: CallbackContext) -> None:
+    set_timer(update, context, WASHER_ONE)
 
 
 def set_timer_washer_two(update: Update, context: CallbackContext) -> None:
     set_timer(update, context, WASHER_TWO)
 
+
 def status(update: Update, context: CallbackContext) -> None:
-  global WASHER_ONE, WASHER_TWO, DRYER_ONE, DRYER_TWO
+    DRYER_ONE_TIMER = DRYER_ONE.status()
+    DRYER_TWO_TIMER = DRYER_TWO.status()
+    WASHER_ONE_TIMER = WASHER_ONE.status()
+    WASHER_TWO_TIMER = WASHER_TWO.status()
 
-  DRYER_ONE_TIMER = DRYER_ONE.status()
-  DRYER_TWO_TIMER = DRYER_TWO.status()
-  WASHER_ONE_TIMER = WASHER_ONE.status()
-  WASHER_TWO_TIMER = WASHER_TWO.status()
-
-  reply_text = f'''Status of Laundry Machines:
+    reply_text = f"""Status of Laundry Machines:
   
 Dryer One: {DRYER_ONE_TIMER}
   
@@ -273,17 +283,17 @@ Dryer Two: {DRYER_TWO_TIMER}
   
 Washer One: {WASHER_ONE_TIMER}
   
-Washer Two: {WASHER_TWO_TIMER}'''
+Washer Two: {WASHER_TWO_TIMER}"""
 
-  # Don't allow users to use /status command in group chats
-  if update.message.chat.type != 'private':
-    Tbot.send_message(
-      chat_id=update.message.from_user.id,
-      text=
-      f"""Hi @{update.message.from_user.username} ,thanks for calling me in the groupchat. \n\nTo prevent spamming in the group, I have sent you a private message instead!\n\n{reply_text}"""
-    )
-    return MENU
-  update.message.reply_text(reply_text)
+    # Don't allow users to use /status command in group chats
+    if update.message.chat.type != "private":
+        TBOT.send_message(
+            chat_id=update.message.from_user.id,
+            text=f"""Hi @{update.message.from_user.username} ,thanks for calling me in the groupchat. \n\nTo prevent spamming in the group, I have sent you a private message instead!\n\n{reply_text}""",
+        )
+        return MENU
+    update.message.reply_text(reply_text)
 
-if __name__ == '__main__':
-  main()
+
+if __name__ == "__main__":
+    main()
